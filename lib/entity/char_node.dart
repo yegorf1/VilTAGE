a77ef6292c795281d7434c15a78cc36bc6edef7d
ViliX64 <vilem.zouhar@gmail.com>
2014-09-21 08:20:19 +0200

    CharNode has now a backgroundColor variable

--- lib/entity/char_node.dart
+++ lib/entity/char_node.dart
@@ -4,6 +4,7 @@ class CharNode {
   int x, y;
   String char = "x";
   String color = "#8989B3";
+  String backgroundColor = "#00FFFFFF";
   List<String> classes = new List<String>();
   
   
@@ -17,6 +18,6 @@ class CharNode {
     for(int i = 0; i < classes.length; i++) {
       sbClasses.write('${classes[i]} ');
     }
-    return '<span class="${sbClasses.toString()}" style="color: $color">$char</span>';
+    return '<span class="${sbClasses.toString()}" style="color: $color; background-color: ${backgroundColor};">$char</span>';
   }
 }
