47310cd48d1b381b3736039ecb7e59f4b54f9339
Vilém Zouhar <vilem.zouhar@gmail.com>
2014-10-26 18:41:00 +0100

    changed initial updatePS

--- lib/viltage_config.dart
+++ lib/viltage_config.dart
@@ -3,7 +3,7 @@ part of viltage;
 class VilTAGEConfig {
   int width = 20;
   int height = 20;
-  int updatePS = 15;
+  int updatePS = 120;
   int fontSize = 14;
   double lineHeight = 1.0;
   String backgroundColor = "#FFFFFF";
