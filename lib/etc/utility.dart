8341c3978b0c893fb3427c605b95f43754eb1244
Vilém Zouhar <vilem.zouhar@gmail.com>
2014-10-27 09:13:43 +0100

    added array comparison + merge

--- lib/etc/utility.dart
+++ lib/etc/utility.dart
@@ -12,22 +12,15 @@ class Utility {
   static draw(ParagraphElement pe, List<List<SpanElement>> charArray) {
     pe.children.clear();
     List<SpanElement> spanElements = new List<SpanElement>();
-//    StringBuffer sb = new StringBuffer();
     for(int i = 0; i < charArray.length; i++) {
       for(int j = 0; j < charArray[0].length; j++) {
         spanElements.add(charArray[i][j]);
-/*        if(charArray[i][j] == null) sb.write("&nbsp;");
-        else if(charArray[i][j] == "") sb.write("&nbsp;");
-        else if(charArray[i][j] == " ") sb.write("&nbsp;");
-        else sb.write(charArray[i][j]); */
       }
       SpanElement endLineElement = new SpanElement();
       endLineElement.setInnerHtml("</br>");
       spanElements.add(endLineElement);
-//      sb.write("<br />");
     }
     for(int i = 0; i < spanElements.length; i++) pe.children.add(spanElements[i]);
-//    pe.setInnerHtml(sb.toString(), validator:nvb);
   }
   
   static update(List<List<Entity>> entities, double delta) {
@@ -38,7 +31,7 @@ class Utility {
     }
   }
   
-  static bool identical(List<List<String>> charArray, List<List<String>> charArray2) {
+  static bool identical(List<List<SpanElement>> charArray, List<List<SpanElement>> charArray2) {
     for(int i = 0; i < charArray.length; i++) {
       for(int j = 0; j < charArray[i].length; j++) {
         if(charArray[i][j] != charArray2[i][j]) return false;
@@ -47,7 +40,7 @@ class Utility {
     return true;
   }
   
-  static void merge(List<List<String>> charArray, List<List<String>> charArray2) {
+  static void merge(List<List<SpanElement>> charArray, List<List<SpanElement>> charArray2) {
     for(int i = 0; i < charArray.length; i++) {
       for(int j = 0; j < charArray[i].length; j++) {
         charArray2[i][j] = charArray[i][j];
