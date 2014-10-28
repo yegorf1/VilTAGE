1c4e72f4af0751ba78a2e4597f33771ce5766dfd
ViliX64 <vilem.zouhar@gmail.com>
2014-10-28 20:14:34 +0100

    added calculation method

--- lib/viltage_config.dart
+++ lib/viltage_config.dart
@@ -5,14 +5,23 @@ class VilTAGEConfig {
   int height = 20;
   int updatePS = 15;
   int fontSize = 14;
-  int offX = 9;
-  int offY = 11;
+  int offX, offY;
+  int canvasWidth = 300;
+  int canvasHeight = 300;
+    
   double lineHeight = 1.0;
   String backgroundColor = "#FFFFFF";
   String backgroundChar = ".";
   CanvasElement ce;
   
   VilTAGEConfig(this.ce) { 
-    offX = offY = fontSize;
+//    offX = offY = fontSize;
+  }
+  
+  calculate() {
+    ce.width = canvasWidth;
+    ce.height = canvasHeight;
+    if(offX == null) offX = canvasWidth~/width;
+    if(offY == null) offY = canvasHeight~/height;
   }
 }
