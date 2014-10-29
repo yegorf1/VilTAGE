82c149159553df8ab2d7d7cb2ace394eb3fe92d7
ViliX64 <vilem.zouhar@gmail.com>
2014-10-29 11:13:21 +0100

    added isPressed method to Input class

--- lib/input.dart
+++ lib/input.dart
@@ -2,7 +2,8 @@ part of viltage;
 
 class Input {
   static HashMap<int, int> _keys = new HashMap<int, int>();
-
+  static HashMap<int, bool> _pressedKeys = new HashMap<int, bool>();
+  
   static init() {
     window.onKeyDown.listen((KeyboardEvent e) {
       if (!_keys.containsKey(e.keyCode))
@@ -15,4 +16,16 @@ class Input {
   }
 
   static bool isHeld(int keyCode) => _keys.containsKey(keyCode);
+  
+  static bool isPressed(int keyCode) {
+    if(!isHeld(keyCode)) {
+      _pressedKeys.remove(keyCode);
+      return false;
+    }
+
+    if(isHeld(keyCode) && !_pressedKeys.containsKey(keyCode)) {
+      _pressedKeys[keyCode] = true;
+      return true;
+    } else return false;
+  }
 }
